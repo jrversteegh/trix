dba17b1278ee308e93e9d465c58684e80df6309b
Jaap Versteegh <j.r.versteegh@gmail.com>
2025-06-20 01:14:14 +0200

    Setup testing of matrix operations

--- include/trix/matrix.h
+++ include/trix/matrix.h
@@ -4,44 +4,55 @@
 #include <ostream>
 #include <sstream>
 #include <random>
+#include <type_traits>
 
 #include "config.h"
 
 namespace trix {
 
-struct MatrixConcept {};
+struct MatrixType {};
 
-struct StorageConcept {};
+struct StorageType {};
 
-template <size_t N, size_t M, typename T = Number>
-struct ArrayStorage : public StorageConcept {
+struct SymmetricType {};
+
+template <typename T>
+concept Scalar = std::is_integral_v<T> || std::is_floating_point_v<T>;
+
+template <typename STORAGE, size_t N, size_t M, size_t SIZE, typename T = Number>
+struct ArrayStorage : public StorageType {
   static constexpr size_t row_count = N;
   static constexpr size_t column_count = M;
-  static constexpr size_t element_count = row_count * column_count;
+  static constexpr size_t element_count = SIZE;
   template <std::convertible_to<T>... Values>
   constexpr ArrayStorage(Values... values) : m_{values...} {};
+  constexpr ArrayStorage(ArrayStorage const&) = default;
+  constexpr ArrayStorage(ArrayStorage&&) = default;
   constexpr T operator[](const size_t i, const size_t j) const {
-    size_t offset = i * M + j;
-    assert(offset < element_count);
-    return m_[offset];
+    return m_[check_and_get_offset_(i, j)];
   }
   constexpr T &operator[](const size_t i, const size_t j) {
-    size_t offset = i * M + j;
-    assert(offset < element_count);
-    return m_[offset];
+    return m_[check_and_get_offset_(i, j)];
   }
   constexpr size_t size() const { return element_count; }
 
 private:
   std::array<T, element_count> m_{};
+  static constexpr size_t check_and_get_offset_(const size_t i, const size_t j) {
+    size_t offset = STORAGE::get_offset(i, j);
+    assert(offset < element_count);
+    return offset;
+  }
 };
 
-template <size_t N, size_t M, typename T = Number,
-          template <size_t P, size_t Q, typename C = Number> typename Storage =
-              ArrayStorage>
-requires (N > 0 && M > 0)
-struct Matrix : public MatrixConcept, public Storage<N, M, T> {
-  using Storage<N, M, T>::Storage;
+template <size_t N, size_t M, typename T = Number>
+struct GenericStorage : public ArrayStorage<GenericStorage<N, M, T>, N, M, N * M, T> {
+  using ArrayStorage<GenericStorage<N, M, T>, N, M, N * M, T>::ArrayStorage;
+  constexpr GenericStorage(GenericStorage const&) = default;
+  constexpr GenericStorage(GenericStorage&&) = default;
+  static constexpr size_t get_offset(const size_t i, const size_t j) {
+    return i * M + j;
+  }
 
   template <typename F>
     requires std::same_as<std::invoke_result_t<F, size_t, size_t>, bool>
@@ -64,63 +75,141 @@ struct Matrix : public MatrixConcept, public Storage<N, M, T> {
       }
     }
   }
+};
 
-  constexpr Matrix &operator+=(Matrix const &other) {
-    for_each_element(
+template <size_t N, size_t M=N, typename T = Number>
+requires (M == N)
+struct SymmetricStorage : public ArrayStorage<SymmetricStorage<N, M, T>, N, M, N * (N + 1) / 2, T>, public SymmetricType {
+  using ArrayStorage<SymmetricStorage<N, M, T>, N, M, N * (N + 1) / 2, T>::ArrayStorage;
+  constexpr SymmetricStorage(SymmetricStorage const&) = default;
+  constexpr SymmetricStorage(SymmetricStorage&&) = default;
+  static constexpr size_t get_offset(const size_t i, const size_t j) {
+    return i > j ? i * (i + 1) / 2 + j : j * (j + 1) / 2 + i;
+  }
+
+  template <typename F>
+    requires std::same_as<std::invoke_result_t<F, size_t, size_t>, bool>
+  constexpr bool for_each_element_while_true(F fun) const {
+    for (size_t i = 0; i < N; ++i) {
+      for (size_t j = 0; j <= i; ++j) {
+        if (!fun(i, j))
+          return false;
+      }
+    }
+    return true;
+  }
+
+  template <typename F>
+    requires std::same_as<std::invoke_result_t<F, size_t, size_t>, void>
+  constexpr void for_each_element(F fun) {
+    for (size_t i = 0; i < N; ++i) {
+      for (size_t j = 0; j <= i; ++j) {
+        fun(i, j);
+      }
+    }
+  }
+};
+
+template <size_t N, size_t M=N, typename T = Number,
+          template <size_t, size_t, typename C> typename Storage =
+              GenericStorage>
+requires (N > 0 && M > 0)
+struct Matrix : public MatrixType, public Storage<N, M, T> {
+  using Storage<N, M, T>::Storage;
+  constexpr Matrix(Matrix const&) = default;
+  constexpr Matrix(Matrix&&) = default;
+  template <typename TO, template<size_t, size_t, typename> typename SO>
+  constexpr Matrix(Matrix<N, M, TO, SO> const& other): Storage<N, M, T>{} {
+    this->for_each_element(
+        [this, &other](size_t i, size_t j) { (*this)[i, j] = other[i, j]; });
+  }
+
+  template <typename TO, template<size_t, size_t, typename> typename SO>
+  constexpr Matrix &operator+=(Matrix<N, M, TO, SO> const &other) {
+    this->for_each_element(
         [this, &other](size_t i, size_t j) { (*this)[i, j] += other[i, j]; });
     return *this;
   }
 
-  constexpr Matrix &operator-=(Matrix const &other) {
-    for_each_element(
+  template <typename TO, template<size_t, size_t, typename> typename SO>
+  constexpr Matrix &operator-=(Matrix<N, M, TO, SO> const &other) {
+    this->for_each_element(
         [this, &other](size_t i, size_t j) { (*this)[i, j] -= other[i, j]; });
     return *this;
   }
 
-  constexpr Matrix &operator*=(T const value) {
-    for_each_element(
+  constexpr Matrix &operator*=(Scalar auto const value) {
+    this->for_each_element(
         [this, value](size_t i, size_t j) { (*this)[i, j] *= value; });
     return *this;
   }
 
   constexpr bool operator==(Matrix const &other) const {
-    return for_each_element_while_true(
+    return this->for_each_element_while_true(
         [this, &other](size_t i, size_t j) -> bool {
           return (*this)[i, j] == other[i, j];
         });
   }
 
+  template <typename TO, template<size_t, size_t, typename> typename SO>
+  constexpr bool operator==(Matrix<N, M, TO, SO> const &other) const {
+    for (size_t i = 0; i < N; ++i) {
+      for (size_t j = 0; j < M; ++j) {
+        if ((*this)[i, j] != other[i, j])
+          return false;
+      }
+    }
+    return true;
+  }
+
 };
 
-template <size_t N, size_t M>
-constexpr auto operator+(Matrix<N, M> const &m1, Matrix<N, M> const &m2) {
-  Matrix<N, M> result{m1};
+template <size_t N, typename T = Number>
+using SymmetricMatrix = Matrix<N, N, T, SymmetricStorage>;
+
+template <size_t N, size_t M, typename T1, typename T2, template <size_t, size_t, typename> typename S1, template <size_t, size_t, typename> typename S2>
+constexpr auto operator+(Matrix<N, M, T1, S1> const &m1, Matrix<N, M, T2, S2> const &m2) {
+  Matrix<N, M, T1> result{m1};
   result += m2;
   return result;
 }
 
-template <size_t N, size_t M>
-constexpr auto operator-(Matrix<N, M> const &m1, Matrix<N, M> const &m2) {
-  Matrix<N, M> result{m1};
+template <size_t N, typename T>
+constexpr auto operator+(SymmetricMatrix<N, T> const &m1, SymmetricMatrix<N, T> const &m2) {
+  SymmetricMatrix<N, T> result{m1};
+  result += m2;
+  return result;
+}
+
+template <size_t N, size_t M, typename T1, typename T2, template <size_t, size_t, typename> typename S1, template <size_t, size_t, typename> typename S2>
+constexpr auto operator-(Matrix<N, M, T1, S1> const &m1, Matrix<N, M, T2, S2> const &m2) {
+  Matrix<N, M, T1> result{m1};
   result -= m2;
   return result;
 }
 
-template <size_t N, size_t M, typename T = Number>
-constexpr auto operator*(Matrix<N, M> const &m, T const value) {
-  Matrix<N, M> result{m};
+template <size_t N, typename T>
+constexpr auto operator-(SymmetricMatrix<N, T> const &m1, SymmetricMatrix<N, T> const &m2) {
+  SymmetricMatrix<N, T> result{m1};
+  result -= m2;
+  return result;
+}
+
+template <size_t N, size_t M, typename T, template <size_t, size_t, typename> typename S, Scalar V>
+constexpr auto operator*(Matrix<N, M, T, S> const &m, V const value) {
+  Matrix<N, M, T, S> result{m};
   result *= value;
   return result;
 }
 
-template <size_t N, size_t M, typename T = Number>
-constexpr auto operator*(T const value, Matrix<N, M> const &m) {
+template <size_t N, size_t M, typename T, template <size_t, size_t, typename> typename S, Scalar V>
+constexpr auto operator*(V const value, Matrix<N, M, T, S> const &m) {
   return m * value;
 }
 
-template <size_t N, size_t M, size_t P>
-constexpr auto operator*(Matrix<N, M> const &m1, Matrix<M, P> const &m2) {
-  Matrix<N, P> result;
+template <size_t N, size_t M, size_t P, typename T, template <size_t, size_t, typename> typename S1, template <size_t, size_t, typename> typename S2>
+constexpr auto operator*(Matrix<N, M, T, S1> const &m1, Matrix<M, P, T, S2> const &m2) {
+  Matrix<N, P, T> result;
   for (size_t i = 0; i < N; ++i) {
     for (size_t j = 0; j < P; ++j) {
       result[i, j] = m1[i, 0] * m2[0, j];
@@ -132,7 +221,21 @@ constexpr auto operator*(Matrix<N, M> const &m1, Matrix<M, P> const &m2) {
   return result;
 }
 
-template <std::convertible_to<MatrixConcept> M>
+template <size_t N, typename T>
+constexpr auto operator*(SymmetricMatrix<N, T> const &m1, SymmetricMatrix<N, T> const &m2) {
+  SymmetricMatrix<N, T> result;
+  for (size_t i = 0; i < N; ++i) {
+    for (size_t j = 0; j <= i; ++j) {
+      result[i, j] = m1[i, 0] * m2[0, j];
+      for (size_t k = 1; k < N; ++k) {
+        result[i, j] += m1[i, k] * m2[k, j];
+      }
+    }
+  }
+  return result;
+}
+
+template <std::convertible_to<MatrixType> M>
 std::ostream &operator<<(std::ostream &out, M const &m) {
   for (size_t i = 0; i < M::row_count; ++i) {
     for (size_t j = 0; j < M::column_count; ++j) {
--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -1,8 +1,5 @@
-#define BOOST_TEST_MODULE matrix_test
-
-//#include <boost/test/included/unit_test.hpp>
-//#include <boost/test/parameterized_test.hpp>
 #include "test_common.h"
+#include <boost/test/parameterized_test.hpp>
 
 #include "trix/matrix.h"
 
@@ -38,9 +35,150 @@ ut::test_suite* init_unit_test_suite(int, char*[]) {
 }
 */
 
-BOOST_AUTO_TEST_SUITE(cpptest);
+struct M {
+  Matrix<3,4> m43{
+    1.0, 2.0, 3.0, 4.0,
+    2.0, 3.0, 4.0, 5.0,
+    3.0, 4.0, 5.0, 6.0,
+  };
+  Matrix<4,3> m34{
+    1.0, 2.0, 3.0,
+    2.0, 3.0, 4.0,
+    3.0, 4.0, 5.0,
+    4.0, 5.0, 6.0,
+  };
+  Matrix<3,3> e33{
+    30.0, 40.0, 50.0,
+    40.0, 54.0, 68.0,
+    50.0, 68.0, 86.0,
+  };
+  Matrix<4,4> e44{
+    14.0, 20.0, 26.0, 32.0,
+    20.0, 29.0, 38.0, 47.0,
+    26.0, 38.0, 50.0, 62.0,
+    32.0, 47.0, 62.0, 77.0,
+  };
+};
 
-BOOST_AUTO_TEST_CASE(matrix_mul_test) {
+
+BOOST_FIXTURE_TEST_CASE(matrix_mul_test3, M) {
+  auto r = m43 * m34;
+  BOOST_TEST(r.row_count == 3);
+  BOOST_TEST(r.column_count == 3);
+  BOOST_TEST(r == e33);
+};
+
+BOOST_FIXTURE_TEST_CASE(matrix_mul_test4, M) {
+  auto r = m34 * m43;
+  BOOST_TEST(r.row_count == 4);
+  BOOST_TEST(r.column_count == 4);
+  BOOST_TEST(r == e44);
+};
+
+BOOST_FIXTURE_TEST_CASE(matrix_scalar_mul_test, M) {
+  auto r1 = 2 * m34;
+  auto r2 = m34 * 2;
+  auto e = m34 + m34;
+  BOOST_TEST(r1 == e);
+  BOOST_TEST(r2 == e);
+};
+
+
+struct SymmetricMatrixFixture {
+
+  SymmetricMatrix<3> symmetric {
+    1.0,
+    2.0, 3.0,
+    3.0, 4.0, 5.0,
+  };
+
+  Matrix<3, 3> square{symmetric};
+  Matrix<3, 3> square_asymmetric{
+    1.0, 2.0, 4.0,
+    2.0, 3.0, 4.0,
+    3.0, 4.0, 5.0,
+  };
+
+  SymmetricMatrix<3> symmetric_multiplied{
+    14.,
+    20., 29.,
+    26., 38., 50.,
+  };
+
+  Matrix<3, 3> square_multiplied{symmetric_multiplied};
+
+  SymmetricMatrix<3> symmetric_added{2 * symmetric};
+  Matrix<3, 3> square_added{symmetric_added};
+
+  SymmetricMatrix<3> symmetric_zero{};
+  Matrix<3, 3> square_zero{};
+};
+
+template <typename ARG1, typename ARG2>
+struct Add {
+  auto operator()(ARG1 const& arg1, ARG2 const& arg2) {
+    return arg1 + arg2;
+  }
+};
+
+template <typename ARG1, typename ARG2>
+struct Subtract {
+  auto operator()(ARG1 const& arg1, ARG2 const& arg2) {
+    return arg1 - arg2;
+  }
+};
+
+template <typename ARG1, typename ARG2>
+struct Multiply {
+  auto operator()(ARG1 const& arg1, ARG2 const& arg2) {
+    return arg1 * arg2;
+  }
+};
+
+template <template<typename, typename> typename OPERATION>
+struct OperationTest {
+  template <typename ARG1, typename ARG2, typename EXPECTED>
+  void operator()(ARG1 const& arg1, ARG2 const& arg2, EXPECTED const& expected) {
+    OPERATION<ARG1, ARG2> operation{};
+    auto r = operation(arg1, arg2);
+    BOOST_TEST((typeid(r) == typeid(expected)));
+    BOOST_TEST(r == expected);
+  }
+};
+
+void do_cross_test(auto& test, auto arg1, auto arg2, auto expected1, auto expected2) {
+  test(arg1, arg1, expected1);
+  test(arg1, arg2, expected1);
+  test(arg2, arg1, expected1);
+  test(arg2, arg2, expected2);
 }
 
-BOOST_AUTO_TEST_SUITE_END();
+BOOST_FIXTURE_TEST_CASE(matrix_symmetric_equality, SymmetricMatrixFixture) {
+  BOOST_TEST(symmetric == symmetric);
+  BOOST_TEST(square == symmetric);
+  BOOST_TEST(symmetric == square);
+  BOOST_TEST(symmetric != square_asymmetric);
+};
+
+BOOST_FIXTURE_TEST_CASE(matrix_symmetric_multiply, SymmetricMatrixFixture) {
+  OperationTest<Multiply> test{};
+  do_cross_test(test, square, symmetric, square_multiplied, symmetric_multiplied);
+};
+
+BOOST_FIXTURE_TEST_CASE(matrix_symmetric_add, SymmetricMatrixFixture) {
+  OperationTest<Add> test{};
+  do_cross_test(test, square, symmetric, square_added, symmetric_added);
+};
+
+BOOST_FIXTURE_TEST_CASE(matrix_symmetric_subtract, SymmetricMatrixFixture) {
+  OperationTest<Subtract> test{};
+  do_cross_test(test, square, symmetric, square_zero, symmetric_zero);
+};
+
+
+
+ut::test_suite* init_unit_test_suite(int, char*[]) {
+//  ut::framework::master_test_suite().add(BOOST_PARAM_TEST_CASE(
+//      &test_qmq_conversion, uqs, uqs + sizeof(uqs) / sizeof(uqs[0])));
+  return 0;
+}
